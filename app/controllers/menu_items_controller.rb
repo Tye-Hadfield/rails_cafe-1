2d6163360f97674dda8049614226cf0a4eba7b15
Tye-Hadfield <tye_hadfield@hotmail.com>
2020-11-03 11:04:58 +1000

    update

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,5 +1,8 @@
-class MenuItemsController < ApplicationController
-  before_action :set_item, only: [:show, :update, :edit, :destroy]
+class MenuItemsController < ApplicationController 
+  skip_before_action :verify_authenticity_token, only: [:buy]
+  before_action :authenticate_user!, except: [:buy]
+  before_action :check_roles, except: [:buy]
+  before_action :set_item, only: [:show, :update, :edit, :destroy, :buy]
 
   def index
     @items = MenuItem.order(:item)
@@ -15,6 +18,7 @@ class MenuItemsController < ApplicationController
 
   def new
     @item = MenuItem.new
+    @menus = Menu.all
   end
 
   def update
@@ -30,14 +34,60 @@ class MenuItemsController < ApplicationController
     redirect_to menu_items_path
   end
 
+
+  def buy
+    p @item
+    Stripe.api_key = ENV['STRIPE_API_KEY']
+    session = Stripe::Checkout::Session.create({
+      payment_method_types: ['card'], 
+      mode: 'payment',
+      success_url: success_url(params[:id]),
+      cancel_url: cancel_url(params[:id]),
+      line_items: [
+        {
+          price_data: {
+            currency: 'aud',
+            product_data: {
+              name: @item.item
+            },
+            unit_amount: (@item.price.to_f * 100).to_i
+          },
+          quantity: 1
+        }
+      ]
+    })
+
+    render json: session
+  end
+
+def success
+  render plain: "Success!"
+end
+
+def cancel
+  render plain: "The Transactions was cancelled!"
+end
+
+
+
+
+
   private
 
   def set_item
     @menus = Menu.all
     @item = MenuItem.find(params[:id])
   end
+  
+
+  def check_roles
+   if !(user_signed_in? && current_user.has_role?(:admin)) 
+    flash[:alert] = "You are not authorized to view this page"
+    redirect_to root_path
+   end
+  end
 
   def menu_item_params
-    params.require(:menu_item).permit(:item, :price, :quantity, menu_ids: [])
+    params.require(:menu_item).permit(:item, :price, :quantity, :image, menu_ids: [])
   end
 end
